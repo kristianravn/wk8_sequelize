08709b50705e998014a3794fc54003a6d85b05fb
jellybeans macbook pro <password1234@passwords-MacBook-Pro.local>
2024-09-17 15:09:33 +0100

    add create for addbook route

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,9 +1,10 @@
 const {Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(process.env.MYSQL_URL, {
+const sequelize = new Sequelize(process.env.MYSQL_URI, {
     pool: {
         max:5,
         min:0,
+        // 30,000 is 30 seconds in code same for 10,000 is 10 seconds
         acquire: 30000,
         idle: 10000,
     },
--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@ const express = require("express");
 
 const bookRouter = require("./books/routes");
 
+const Book = require("./books/model");
+
 const port = process.env.PORT || 5001;
 
 const app = express();
@@ -11,10 +13,15 @@ app.use(express.json());
 
 app.use(bookRouter);
 
+const syncTables = () =>{
+    Book.sync();
+}
+
 app.get("/health", (req, res) =>{
     res.status(200).json({message: "api is healthy"});
 });
 
-app.listen(5001, () =>{
+app.listen(port, () =>{
+    syncTables ();
     console.log(`servser is listening on port ${port}`);
 });
